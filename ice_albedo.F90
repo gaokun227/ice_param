210a4d3a23578a336e50f77e180cc7cba41ab62b
Seth Underwood <Seth.Underwood@noaa.gov>
2015-01-09 14:26:41 -0500

    Removed CVS keyword expansions

--- ice_albedo.F90
+++ ice_albedo.F90
@@ -29,8 +29,8 @@ public  ice_albedo, ice_albedo_init
 
 !--------------------- version number ----------------------------------
 
-character(len=128) :: version = '$Id: ice_albedo.F90,v 19.0 2012/01/06 20:36:39 fms Exp $'
-character(len=128) :: tagname = '$Name: testing $'
+character(len=128) :: version = '$Id$'
+character(len=128) :: tagname = '$Name$'
 
 !=======================================================================
 
--- ocean_rough.F90
+++ ocean_rough.F90
@@ -20,8 +20,8 @@ private
 public :: compute_ocean_roughness, fixed_ocean_roughness
 
 !-----------------------------------------------------------------------
-character(len=256) :: version = '$Id: ocean_rough.F90,v 19.0.10.1 2014/05/16 18:45:48 Lucas.Harris Exp $'
-character(len=256) :: tagname = '$Name: testing $'
+character(len=256) :: version = '$Id$'
+character(len=256) :: tagname = '$Name$'
 !-----------------------------------------------------------------------
 !----- namelist -----
 
