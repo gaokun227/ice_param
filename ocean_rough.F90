647e29c9defd971c9987841572b8998aab8501ea
Bruce Wyman <Bruce.Wyman@noaa.gov>
2015-11-06 12:12:48 -0500

    New 'rough_scheme' option added for 'coare3.5'.

--- ocean_rough.F90
+++ ocean_rough.F90
@@ -12,7 +12,7 @@ use          fms_mod, only: open_namelist_file
 use       fms_mod, only: error_mesg, FATAL, file_exist,  &
                          check_nml_error, mpp_pe, mpp_root_pe, close_file, &
                          write_version_number, stdlog
-use constants_mod, only: grav
+use constants_mod, only: grav, vonkarm
 
 implicit none
 private
@@ -20,8 +20,8 @@ private
 public :: compute_ocean_roughness, fixed_ocean_roughness
 
 !-----------------------------------------------------------------------
-character(len=256) :: version = '$Id$'
-character(len=256) :: tagname = '$Name$'
+character(len=256) :: version = '$Id: ocean_rough.F90,v 19.0.10.1 2014/05/16 18:45:48 Lucas.Harris Exp $'
+character(len=256) :: tagname = '$Name: ulm_201505 $'
 !-----------------------------------------------------------------------
 !----- namelist -----
 
@@ -90,8 +90,10 @@ contains
 !-----------------------------------------------------------------------
 
    real, dimension(size(ocean,1),size(ocean,2)) :: ustar2, xx1, xx2, w10 !miz
+   real, dimension(size(ocean,1),size(ocean,2)) :: ustar, xx3, u10n, z0, z1, alpha_v, reynolds_rough
    real:: zt1
-   integer :: i, j
+   integer :: i, j, n, iter
+   real :: ustar_min, m, b, u_max, rough_mom_init
 
    if (do_init) call ocean_rough_init
 
@@ -165,6 +167,53 @@ contains
           endwhere
           endif
       endif
+
+   else if (trim(rough_scheme) == 'coare3.5') then    !IH -ref Edson et al, JPO, Aug 2013 
+
+      !if rough_mom is available in input (rough_mon_in) then set rough_mom1_init = rough_mom_in
+      rough_mom_init = 1.e-03
+      ustar_min = 1.e-05 ! added by IH just in case ustar is unphysically small 
+      m = 0.0017         !Edson 2013
+      b = -0.005         !Edson 2013
+      u_max = 18.0       !Edson 2013
+      ustar(:,:)=u_star(:,:)
+      where (ocean)
+          ustar(:,:)  = max(ustar(:,:), ustar_min)  ! IH
+	  ustar2(:,:) = ustar(:,:)*ustar(:,:)
+          xx1(:,:)    = gnu/ustar(:,:)
+          xx2(:,:)    = ustar2(:,:)/grav
+	  xx3(:,:)    = ustar(:,:)/vonkarm 
+      endwhere	  
+      !if rough_mom is available in input then set z0(:,:) to this input value
+      z0(:,:) = rough_mom_init
+      iter = 5      ! IH - overkill
+      do j=1,size(ocean,2)
+     	do i=1,size(ocean,1)
+           if ( ocean(i,j) ) then
+              do n = 1, iter
+	      	 u10n(i,j) = xx3(i,j)*log(10/z0(i,j))             ! "neutral" 10m wind
+	    	 alpha_v(i,j) = m*min(u10n(i,j),u_max) + b;     ! Charnock coefficient, Edson 2013
+	    	 z1(i,j) = zcom2*xx1(i,j) + alpha_v(i,j)*xx2(i,j) ! Edson 2013
+	    	 z0(i,j) = z1(i,j);                               !IH -- iteration
+	      enddo
+           endif
+        enddo
+      enddo
+
+      where (ocean)
+      	  rough_mom  (:,:) = z0(:,:)
+      	  rough_mom  (:,:) = max( rough_mom  (:,:), roughness_min )
+
+	  reynolds_rough(:,:) = ustar(:,:)*rough_mom(:,:)/gnu
+	  rough_heat (:,:)    = 5.5e-05*(reynolds_rough(:,:)**(-0.6))
+	  rough_heat (:,:)    = min(1.1e-04, rough_heat(:,:))
+	  rough_moist(:,:)    = rough_heat (:,:)
+      elsewhere
+          rough_mom   = 0.0
+          rough_heat  = 0.0
+          rough_moist = 0.0
+      endwhere
+
    endif
 
 !-----------------------------------------------------------------------
